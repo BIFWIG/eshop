eb59cc7aa847622d1703f388d5aae8c0e4f22219
bif <wf19941211@gmail.com>
2021-01-11 13:56:26 +0800

    初始化springApplication

---
+++ src/main/java/cn/bif/Application.java
@@ -0,0 +1,17 @@
+
+package cn.bif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ted Wang
+ * @created 2021/1/11 1:53 下午
+ */
+@SpringBootApplication
+public class Application {
+    
+    public static void main (String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
